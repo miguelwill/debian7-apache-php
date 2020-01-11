3543b1bbef5ee98ca8099161a9a01d3b74a43b9e
miguelwill <miguelwill@gmail.com>
2020-01-11 16:16:21 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -9,13 +9,13 @@ cat /tmp/le > /etc/apache2/sites-enabled/default-ssl
 rm -f /etc/apache2/conf.d/other-vhosts-access-log
 
 sed -i '/output_buffering = 4096/c output_buffering = Off' /etc/php5/apache2/php.ini
-sed -i '/SecRuleEngine DetectionOnly/c SecRuleEngine On' /etc/modsecurity/modsecurity.conf
-sed -i '/SecRequestBodyLimit 13107200/c SecRequestBodyLimit 131072000' /etc/modsecurity/modsecurity.conf
-sed -i '/SecRequestBodyNoFilesLimit 131072/c SecRequestBodyNoFilesLimit 1310720' /etc/modsecurity/modsecurity.conf
-sed -i '/SecRequestBodyInMemoryLimit 131072/c SecRequestBodyInMemoryLimit 1310720' /etc/modsecurity/modsecurity.conf
-sed -i '/SecPcreMatchLimit 1000/c SecPcreMatchLimit 10000' /etc/modsecurity/modsecurity.conf
-sed -i '/SecPcreMatchLimitRecursion 1000/c SecPcreMatchLimitRecursion 10000' /etc/modsecurity/modsecurity.conf
-sed -i '/SecResponseBodyLimit 524288/c SecResponseBodyLimit 5242880' /etc/modsecurity/modsecurity.conf
+#sed -i '/SecRuleEngine DetectionOnly/c SecRuleEngine On' /etc/modsecurity/modsecurity.conf
+#sed -i '/SecRequestBodyLimit 13107200/c SecRequestBodyLimit 131072000' /etc/modsecurity/modsecurity.conf
+#sed -i '/SecRequestBodyNoFilesLimit 131072/c SecRequestBodyNoFilesLimit 1310720' /etc/modsecurity/modsecurity.conf
+#sed -i '/SecRequestBodyInMemoryLimit 131072/c SecRequestBodyInMemoryLimit 1310720' /etc/modsecurity/modsecurity.conf
+#sed -i '/SecPcreMatchLimit 1000/c SecPcreMatchLimit 10000' /etc/modsecurity/modsecurity.conf
+#sed -i '/SecPcreMatchLimitRecursion 1000/c SecPcreMatchLimitRecursion 10000' /etc/modsecurity/modsecurity.conf
+#sed -i '/SecResponseBodyLimit 524288/c SecResponseBodyLimit 5242880' /etc/modsecurity/modsecurity.conf
 
 /etc/init.d/memcached start
 
